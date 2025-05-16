a48bb066f3262d61129d8ea65c0b9b4b6f4a954d
yu <yu@2287>
2025-05-16 13:10:33 +0800

    second commit

--- src/main/java/com/git/test1/test.java
+++ src/main/java/com/git/test1/test.java
@@ -3,5 +3,6 @@ package com.git.test1;
 public class test {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
